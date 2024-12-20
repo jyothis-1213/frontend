0498899e6c5b4984a60327bd7f275c41fcbb2e20
jyothis-1213 <jyothisj1213@gmail.com>
2024-12-20 18:38:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,23 @@
+// import React from 'react';
+// import { StrictMode } from 'react';
+// import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter for routing
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import App from './App.jsx';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>
+// );
+
 import React from 'react';
 import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter for routing
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
